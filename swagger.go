a672f27b937f7fda3e03ba0a3e7f2400410044c4
ilya.timofeev <ilya.timofeev@ru.axxonsoft.com>
2020-05-24 18:21:07 +0300

    * added swagger ValidateSpec method

--- swagger.go
+++ swagger.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"encoding/json"
+	"github.com/go-openapi/loads"
 	"net/http"
 	"net/http/httptest"
 )
@@ -17,4 +19,12 @@ func (s *handlerTransport) RoundTrip(req *http.Request) (*http.Response, error) 
 	resp := httptest.NewRecorder()
 	s.h.ServeHTTP(resp, req)
 	return resp.Result(), nil
+}
+
+func ValidateSpec(orig, flat json.RawMessage) *loads.Document {
+	swaggerSpec, err := loads.Embedded(orig, flat)
+	if err != nil {
+		Log.Fatalln(err)
+	}
+	return swaggerSpec
 }
